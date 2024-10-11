d2c1641e7f0aa9553e41c1d3e03e345da4ca81fc
Haya <hayaa.official10@gmail.com>
2024-10-11 22:13:35 +0500

    Update!

--- Javascript 10/app.js
+++ Javascript 10/app.js
@@ -56,8 +56,38 @@ let c = 7;
 let area = calculateArea(a, b, c);
 document.write("The area of the triangle is: " + area.toFixed(2));
 
+// 4. Write a function that receives marks received by a student in 3
+// subjects and returns the average and percentage of these
+// marks. there should be 3 functions one is the mainFunction
+// and other are for average and percentage. Call those functions
+// from mainFunction and display result in mainFunction.
 
 
+function calculateAverage(marks) {
+    let totalMarks = marks.reduce((sum, mark) => sum + mark, 0);
+    return totalMarks / marks.length; 
+}
+
+
+function calculatePercentage(marks, totalMaxMarks) {
+    let totalMarksObtained = marks.reduce((sum, mark) => sum + mark, 0); 
+    return (totalMarksObtained / totalMaxMarks) * 100;  
+}
+
+
+function mainFunction() {
+    let marks = [80, 90, 85]; 
+
+    let totalMaxMarks = 300;
+
+    let average = calculateAverage(marks);
+    let percentage = calculatePercentage(marks, totalMaxMarks);
+
+    console.log("Average Marks: " + average);
+    console.log("Percentage: " + percentage + "%");
+}
+
+mainFunction();
 
 //5: You have learned the function indexOf. Code your own custom
 // function that will perform the same functionality. You can code
@@ -142,6 +172,40 @@ alert(countVowelPairs("Please read this application and give me gratuity"))
 // keyboard. Write four functions to convert and print this
 // distance in meters, feet, inches and centimeters.
 
+function kilometersToMeters(kilometers) {
+    return kilometers * 1000;
+}
+
+function kilometersToFeet(kilometers) {
+    return kilometers * 3280.84;
+}
+
+
+function kilometersToInches(kilometers) {
+    return kilometers * 39370.1;
+}
+
+function kilometersToCentimeters(kilometers) {
+    return kilometers * 100000;
+}
+
+function displayDistanceConversions() {
+    let kilometers = 5;  
+
+    console.log("Distance in kilometers: " + kilometers + " km");
+    console.log("Distance in meters: " + kilometersToMeters(kilometers) + " meters");
+    console.log("Distance in feet: " + kilometersToFeet(kilometers).toFixed(2) + " feet");
+    console.log("Distance in inches: " + kilometersToInches(kilometers).toFixed(2) + " inches");
+    console.log("Distance in centimeters: " + kilometersToCentimeters(kilometers) + " centimeters");
+}
+n
+displayDistanceConversions();
+
+
+//9: Write a program to calculate overtime pay of employees.
+// Overtime is paid at the rate of Rs. 12.00 per hour for every hour
+// worked above 40 hours. Assume that employees do not work
+// for fractional part of an hour.
 
 function calculateOvertimePay(hoursWorked) {
     const regularHours = 40;
@@ -159,3 +223,43 @@ var hoursWorked = 45;
 var overtimePay = calculateOvertimePay(hoursWorked);
 
 alert(`The overtime pay for ${hoursWorked} hours worked is Rs. ${overtimePay.toFixed(2)}`);
+
+//10: A cashier has currency notes of denominations 10, 50 and
+// 100. If the amount to be withdrawn is input through the
+// keyboard in hundreds, find the total number of currency notes
+// of each denomination the cashier will have to give to the
+// withdrawer.
+
+
+function getCurrencyNotes(amount) {
+    let numHundreds = 0, numFifties = 0, numTens = 0;
+
+    numHundreds = Math.floor(amount / 100);
+    amount = amount % 100;  
+
+    numFifties = Math.floor(amount / 50);
+    amount = amount % 50;
+
+    numTens = Math.floor(amount / 10);
+    amount = amount % 10;  
+
+    return { numHundreds, numFifties, numTens };
+}
+
+
+function displayCurrencyNotes() {
+    let amount = 580;  
+
+    if (amount % 10 !== 0) {
+        console.log("Please enter an amount in multiples of 10.");
+        return;
+    }
+
+    let notes = getCurrencyNotes(amount);
+    console.log("Number of 100 Rs notes: " + notes.numHundreds);
+    console.log("Number of 50 Rs notes: " + notes.numFifties);
+    console.log("Number of 10 Rs notes: " + notes.numTens);
+}
+
+displayCurrencyNotes();
+
